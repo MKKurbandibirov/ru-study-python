e2c5345dff4a76bef683f41d82e634086c989ae6
MKKurbandibirov <MKKurbandibirov@yandex.ru>
2023-01-18 14:51:53 +0300

    Create my own max(); Create recursive b_search

--- lists/lists.py
+++ lists/lists.py
@@ -1,10 +1,19 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
+        def list_max(input_list: list[int]) -> int:
+            maximum = input_list[0]
+
+            for elem in input_list:
+                if maximum < elem:
+                    maximum = elem
+
+            return maximum
+
         if not input_list:
             return input_list
 
-        maximum = max(input_list)
+        maximum = list_max(input_list)
         for ind in range(len(input_list) - 1):
             if input_list[ind] > 0:
                 input_list[ind] = maximum
@@ -13,20 +22,19 @@ class ListExercise:
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        if not input_list:
-            return -1
+        def binary_search(
+            input_list: list[int], query: int, start: int = 0, end: int = len(input_list) - 1
+        ) -> int:
+            if end < start:
+                return -1
 
-        start, end = 0, len(input_list) - 1
-
-        while start <= end:
             mid = (start + end) // 2
 
             if input_list[mid] == query:
                 return mid
-
-            if input_list[mid] > query:
-                end = mid - 1
             elif input_list[mid] < query:
-                start = mid + 1
+                return binary_search(input_list, query, mid + 1, end)
+            else:
+                return binary_search(input_list, query, start, mid - 1)
 
-        return -1
+        return binary_search(input_list, query)
